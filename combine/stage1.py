afb76ef9827f846f1aa89487bd3a72ecc28f873e
Kurakin Alexander <kuraga333@mail.ru>
2017-02-21 09:48:28 +0300

    Комбинировать не ттолько betarch, но и betcity

--- combine/stage1.py
+++ combine/stage1.py
@@ -57,6 +57,30 @@ def betarch_to_universal(metadata):
   return res
 
 
+def betcity_to_universal(metadata):
+  res = {}
+
+  for match_metadata in metadata:
+    team_info = get_team_info_by('betcityName', match_metadata['home'])
+    if team_info is None:
+      continue
+
+    match_home = team_info['whoscoredName']
+    match_away = team_info['whoscoredName']
+    match_date = match_metadata['date']
+    match_uuid = match_metadata['uuid']
+
+    res[match_home] = res.get(match_home, {})
+    res[match_home][match_date] = res[match_home].get(match_date, [])
+    res[match_home][match_date].append(match_uuid)
+
+    res[match_away] = res.get(match_away, {})
+    res[match_away][match_date] = res[match_away].get(match_date, [])
+    res[match_away][match_date].append(match_uuid)
+
+  return res
+
+
 def get_whoscored_matches(match_date, match_uuids):
   matches = []
 
@@ -81,6 +105,18 @@ def get_betarch_matches(match_date, match_uuids):
   return matches
 
 
+def get_betcity_matches(match_date, match_uuids):
+  matches = []
+
+  for match_uuid in match_uuids:
+    file_path = os.path.join('data', 'betcity', 'matchesJson', match_date, '%s.json' % (match_uuid,))
+    with open(file_path, 'r', encoding='utf-8') as f:
+      match = json.load(f)
+    matches.append(match)
+
+  return matches
+
+
 whoscored_metadata_file_path = os.path.join('data', 'whoscored', 'matches_metadata.json')
 with open(whoscored_metadata_file_path, 'r', encoding='utf-8') as f_whoscored_metadata:
   whoscored_metadata = json.load(f_whoscored_metadata)
@@ -90,6 +126,11 @@ betarch_metadata_file_path = os.path.join('data', 'betarch', 'matches_metadata.j
 with open(betarch_metadata_file_path, 'r', encoding='utf-8') as f_betarch_metadata:
   betarch_metadata = json.load(f_betarch_metadata)
 betarch_metadata_grouped = betarch_to_universal(betarch_metadata)
+
+betcity_metadata_file_path = os.path.join('data', 'betcity', 'matches_metadata.json')
+with open(betcity_metadata_file_path, 'r', encoding='utf-8') as f_betcity_metadata:
+  betcity_metadata = json.load(f_betcity_metadata)
+betcity_metadata_grouped = betcity_to_universal(betcity_metadata)
 
 handled_whoscored_uuids = set()
 for team in whoscored_metadata_grouped:
@@ -101,11 +142,13 @@ for team in whoscored_metadata_grouped:
     print('%s - %s' % (team, date_str))
     whoscored_data = get_whoscored_matches(date_str, whoscored_uuids)
 
+    betting_data = []
     if team in betarch_metadata_grouped and date_str in betarch_metadata_grouped[team]:
       betarch_uuids = betarch_metadata_grouped[team][date_str]
-      betarch_data = get_betarch_matches(date_str, betarch_uuids)
-    else:
-      betarch_data = []
+      betting_data += get_betarch_matches(date_str, betarch_uuids)
+    if team in betcity_metadata_grouped and date_str in betcity_metadata_grouped[team]:
+      betcity_uuids = betcity_metadata_grouped[team][date_str]
+      betting_data += get_betcity_matches(date_str, betcity_uuids)
 
     match_uuid_str = str(uuid.uuid4())
     match_data = {
@@ -118,7 +161,7 @@ for team in whoscored_metadata_grouped:
       'seasonId': whoscored_data[0]['seasonId'],
       'stageId': whoscored_data[0]['stageId'],
       'whoscored': whoscored_data,
-      'betarch': betarch_data
+      'betarch': betting_data
     }
     match_name = '%s - %s vs %s' % (match_data['date'], match_data['home'], match_data['away'])
 
