fa6fd69881620584fe4d68af34e62d676a83c349
Alexander Kurakin <kuraga333@mail.ru>
2017-03-10 02:16:41 +0300

    Переделка (8.1 - реализация исходов таймов угловых)

---
+++ betting/proposers/corners_periods_results_proposers.py
@@ -0,0 +1,66 @@
+import numpy as np
+from betting.proposers.match_proposers import CornersMatchProposer
+
+
+class CornersFirstPeriodResults1Proposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.tril(probabilities, k=-1).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (1-й тайм)', None, '1', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersFirstPeriodResults1XProposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.tril(probabilities, k=0).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (1-й тайм)', None, '1X', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersFirstPeriodResultsX2Proposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.triu(probabilities, k=0).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (1-й тайм)', None, 'X2', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersFirstPeriodResults2Proposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.triu(probabilities, k=-1).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (1-й тайм)', None, '2', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersSecondPeriodResults1Proposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.tril(probabilities, k=-1).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (2-й тайм)', None, '1', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersSecondPeriodResults1XProposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.tril(probabilities, k=0).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (2-й тайм)', None, '1X', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersSecondPeriodResultsX2Proposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.triu(probabilities, k=0).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (2-й тайм)', None, 'X2', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
+
+
+class CornersSecondPeriodResults2Proposer(CornersMatchProposer):
+
+    def _handle(self, betcity_match, probabilities, whoscored_match=None, confidence_level=0.6):
+        if np.triu(probabilities, k=-1).sum() / probabilities.sum() > confidence_level:
+            bet_pattern = (None, 'Исходы по таймам (2-й тайм)', None, '2', None)
+            self.propose(bet_pattern, betcity_match, whoscored_match=whoscored_match)
--- experiment.py
+++ experiment.py
@@ -3,12 +3,13 @@ import os
 import pickle
 from betting.provider import Provider
 from betting.experimentor import Experimentor
+from util.common_util import safe_get
 
 from betting.samplers.date_based_samplers import HistoricalSampler, EveSampler
 from betting.fitters.corners_attack_defense_fitters import CornersAttackDefenseFitter
 from betting.predictors.corners_attack_defense_predictors import CornersAttackDefensePredictor
 from betting.proposers.corners_results_proposers import CornersResults1Proposer, CornersResults1XProposer, CornersResultsX2Proposer, CornersResults2Proposer
-from util.common_util import safe_get
+from betting.proposers.corners_periods_results_proposers import CornersFirstPeriodResults1Proposer, CornersFirstPeriodResults1XProposer, CornersFirstPeriodResultsX2Proposer, CornersFirstPeriodResults2Proposer, CornersSecondPeriodResults1Proposer, CornersSecondPeriodResults1XProposer, CornersSecondPeriodResultsX2Proposer, CornersSecondPeriodResults2Proposer
 
 
 def make_experiment(provider, db_name, matches_collection_name, sample_condition, fitter_fitted_data=None):
@@ -60,11 +61,42 @@ corners_results_proposers_data = [{
     'name': 'corners_results-2',
     'proposer': CornersResults2Proposer(threshold=threshold)
 }]
+corners_periods_results_proposers_data = [{
+    'name': 'corners_periods_results-first_period-1',
+    'proposer': CornersFirstPeriodResults1Proposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-first_period-1X',
+    'proposer': CornersFirstPeriodResults1XProposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-first_period-X2',
+    'proposer': CornersFirstPeriodResultsX2Proposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-first_period-2',
+    'proposer': CornersFirstPeriodResults2Proposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-second_period-1',
+    'proposer': CornersSecondPeriodResults1Proposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-second_period-1X',
+    'proposer': CornersSecondPeriodResults1XProposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-second_period-X2',
+    'proposer': CornersSecondPeriodResultsX2Proposer(threshold=threshold)
+}, {
+    'name': 'corners_periods_results-second_period-2',
+    'proposer': CornersSecondPeriodResults2Proposer(threshold=threshold)
+}]
 
 
 # В данном цикле используем `corners_attack_defense_fitter`
 for train_sampler_name, train_sampler in train_samplers.items():
     name = 'provider-corners_results-corners_attack_defense-%s' % (train_sampler_name,)
     description = 'Исходы угловых, предсказание по атаке и обороне команд (угловые, рассматривается вероятность счетов), недавние данные'
-    provider = Provider(name, description, train_sampler, corners_attack_defense_fitter, corners_attack_defense_predictor, corners_results_proposers_data)
-    make_experiment(provider, db_name, matches_collection_name, sample_condition)
+    provider1 = Provider(name, description, train_sampler, corners_attack_defense_fitter, corners_attack_defense_predictor, corners_results_proposers_data)
+    make_experiment(provider1, db_name, matches_collection_name, sample_condition)
+
+
+    name = 'provider-corners_periods_results-corners_periods_attack_defense-%s' % (train_sampler_name,)
+    description = 'Исходы угловых 1-го тайма и 2-го тайма, предсказание по атаке и обороне команд (угловые, рассматривается вероятность счетов), недавние данные'
+    provider2 = Provider(name, description, train_sampler, corners_attack_defense_fitter, corners_attack_defense_predictor, corners_periods_results_proposers_data)
+    make_experiment(provider2, db_name, matches_collection_name, sample_condition)
