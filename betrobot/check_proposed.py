2c32e2a7c6b8bf10857f6d30c9e71b562e17b1ff
Alexander Kurakin <kuraga333@mail.ru>
2017-03-13 15:40:09 +0300

    Переделка (15 - для проверки ставок используем базу, а не файлы)

--- betrobot/check_proposed.py
+++ betrobot/check_proposed.py
@@ -3,41 +3,24 @@ import json
 import glob
 import pymongo
 import datetime
-from betrobot.util.sport_util import get_betcity_teams_of_whoscored_match
 from betrobot.util.check_util import check_bet
 
 
 client = pymongo.MongoClient()
 db = client['betrobot']
-proposed = db['proposed']
+proposed_collection = db['proposed']
+matches_collection = db['matches']
 
 
-def check_whoscored_match(whoscored_match, proposed):
-    whoscored_match_date = datetime.datetime.strptime(whoscored_match['date'], '%Y-%m-%d').replace(hour=0, minute=0, second=0, microsecond=0)
-    (home_betcity, away_betcity) = get_betcity_teams_of_whoscored_match(whoscored_match)
-    if home_betcity is None or away_betcity is None:
-        return
-
-    bets = proposed.find({ 'date': whoscored_match_date, 'home': home_betcity, 'away': away_betcity, 'ground_truth': None })
-
-    for bet in bets:
-        ground_truth = check_bet(bet['bet_pattern'], bet['match_special_word'], whoscored_match)
-        if ground_truth is not None:
-            proposed.update_one({ '_id': bet['_id'] }, { '$set': { 'ground_truth': ground_truth }})
-
-dates = set()
-
-unchecked_bets = proposed.find({ 'ground_truth': None })
+unchecked_bets = proposed_collection.find({ 'ground_truth': None })
 for bet in unchecked_bets:
-    dates.add(bet['date'])
+    date_str = bet['date'].strftime('%Y-%m-%d')
+    print('%s - %s vs %s' % (date_str, bet['home'], bet['away']))
 
-for date_ in dates:
-    date_str = date_.strftime('%Y-%m-%d')
-    glob_path = os.path.join('data', 'whoscored', 'matchesJson', date_str, '*.json')
-    for file_path in glob.iglob(glob_path):
-        print(file_path)
+    match_data = matches_collection.findOne({ 'date': date_str, 'home': bet['home'], 'away': bet['away'] })
+    whoscord_match = match_data['whoscored'][0]
 
-        with open(file_path, 'r', encoding='utf-8') as f:
-            whoscored_match = json.load(f)
+    ground_truth = check_bet(bet['bet_pattern'], bet['match_special_word'], whoscored_match)
+    if ground_truth is not None:
+        proposed_collection.update_one({ '_id': bet['_id'] }, { '$set': { 'ground_truth': ground_truth }})
 
-        check_whoscored_match(whoscored_match, proposed)
