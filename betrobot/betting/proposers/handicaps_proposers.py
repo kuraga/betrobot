0fcf439d1b987deacb82ad5922a6e96e4d6d6d68
Alexander Kurakin <kuraga333@mail.ru>
2017-04-09 01:09:04 +0300

    Внедрены дополнительные форы

--- betrobot/betting/proposers/handicaps_proposers.py
+++ betrobot/betting/proposers/handicaps_proposers.py
@@ -5,9 +5,10 @@ import numpy as np
 class HandicapsHomeProposer(object):
 
     def _handle(self, betcity_match, probabilities, whoscored_match=None):
-        bet_pattern = ('*', 'Фора', betcity_match['home'], None, '*')
+        bet_pattern1 = ('*', 'Фора', betcity_match['home'], None, '*')
+        bet_pattern2 = ('*', 'Дополнительные форы', betcity_match['home'], None, '*')
 
-        bets = get_bets(bet_pattern, betcity_match)
+        bets = get_bets(bet_pattern1, betcity_match) + get_bets(bet_pattern2, betcity_match)
         for bet in bets:
             handicap = bet[4]
             predicted_probability = np.tril(probabilities, k=np.ceil(handicap)-1).sum()
@@ -19,8 +20,9 @@ class HandicapsAwayProposer(object):
 
     def _handle(self, betcity_match, probabilities, whoscored_match=None):
         bet_pattern = ('*', 'Фора', betcity_match['away'], None, '*')
+        bet_pattern2 = ('*', 'Дополнительные форы', betcity_match['away'], None, '*')
 
-        bets = get_bets(bet_pattern, betcity_match)
+        bets = get_bets(bet_pattern1, betcity_match) + get_bets(bet_pattern2, betcity_match)
         for bet in bets:
             handicap = bet[4]
             predicted_probability = np.triu(probabilities, k=-(np.ceil(handicap)-1)).sum()
