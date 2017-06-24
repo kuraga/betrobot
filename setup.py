c18ee02314370701e6f237c8b3c8f973b5efe7e3
Alexander Kurakin <kuraga333@mail.ru>
2017-06-24 22:26:01 +0300

    Улучшения: Оформление пакета

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Specify the Python versions you support here.
         'Programming Language :: Python :: 3.4',
@@ -42,14 +42,6 @@ setup(
 
     # What does your project relate to?
     keywords='sport betting',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -66,27 +58,7 @@ setup(
         'numpy>=1.11.2',
         'pandas>=0.19.0',
         'scipy>=0.18.1',
-        'bottle>=0.12.10'
-    ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    package_data={},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={},
+        'bottle>=0.12.10',
+        'tqdm>=4.11.2'
+    ]
 )
