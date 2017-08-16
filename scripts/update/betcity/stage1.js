d662a9dd386d8cf5b74d27c97dc23eb7f67769a1
Alexander Kurakin <kuraga333@mail.ru>
2017-08-16 11:32:41 +0300

    Скачивание Betcity по частям

--- scripts/update/betcity/stage1.js
+++ scripts/update/betcity/stage1.js
@@ -10,51 +10,90 @@ function _parse_betcity_stage1() {
   var currentDateStr = strftime('%Y-%m-%d', currentDate);
 
   var nightmare = Nightmare({
-    show: true,
-    waitTimeout: 1800000,
-    pollInterval: 60000,
+    show: false,
+    waitTimeout: 600000,
+    pollInterval: 10000,
     switches: {
       'disable-http-cache': true,
       'ignore-certificate-errors': true
     }
   });
 
+
   nightmare
-    .goto('https://www.betsbc.com/new/#/line/line_ids=a:1')
-    .wait(5000)
-    .wait('input[name=simple]')
-    .wait(1000)
+    .goto('https://www.betsbc.com/new/#!/line/line_ids=a:1')
+    .wait(4000)
+
     .select('select[name=time]', '5')
     .wait(1000)
-    .click('input#dop')
-    .wait(1000)
-    .click('input[name=simple]')
-    .wait(1000)
-    .click('a#btn_submit')
-    .wait(60000)
-    .wait(function () {
-      var n = document.getElementsByClassName('loadingExt').length;
-      if (window.lastN === n) {
-        return true;
-      } else {
-        window.lastN = n;
-        return false;
-      }
-    })
-    .wait(10000)
 
     .evaluate(function () {
-      return document.documentElement.outerHTML;
+      var selector = 'td.b1 input[type=checkbox][ng-click=changeOne]';
+      var elements = document.querySelectorAll(selector);
+      return elements.length;
     })
 
-    .end()
+    .then(function (count) {
+      console.log('Found ' + count + ' tournaments');
 
-    .then(function (result) {
-      var matchesHtmlDirPath = path.posix.join('tmp', 'update', 'betcity', 'datesHtml');
-      fs.ensureDirSync(matchesHtmlDirPath);
+      var firsts = [];
+      for (var first = 0; first < count; first += 10) {
+        firsts.push(first);
+      }
 
-      var matchesHtmlFilePath = path.posix.join('tmp', 'update', 'betcity', 'datesHtml', currentDateStr + '.html');
-      fs.writeFileSync(matchesHtmlFilePath, result);
+      firsts.reduce(function(accumulator, first) {
+        return accumulator.then(function (results) {
+          var last = Math.min(first + 10, count);
+
+          console.log('Saving tournaments [' + first + ', ' + last + ')...');
+
+          return nightmare
+            .goto('https://www.betsbc.com/new/#!/line/line_ids=a:1')
+            .wait(4000)
+
+            .select('select[name=time]', '5')
+            .wait(1000)
+
+            .click('input#dop')
+            .wait(1000)
+
+            .evaluate(function (first, last) {
+              var selector = 'td.b1 input[type=checkbox][ng-click=changeOne]';
+              var elements = document.querySelectorAll(selector);
+              for (var i = first; i < last; ++i) {
+                var event = document.createEvent('MouseEvent');
+                event.initEvent('click', true, true);
+                elements[i].dispatchEvent(event);
+              }
+            }, first, last)
+            .wait(5000)
+
+            .click('a#btn_submit')
+            .wait(1000)
+
+            .wait(10000)
+
+            .evaluate(function () {
+              return document.documentElement.outerHTML;
+            })
+            .then(function (result) {
+              var matchesHtmlDirPath = path.posix.join('tmp', 'update', 'betcity', 'datesHtml');
+              fs.ensureDirSync(matchesHtmlDirPath);
+
+              var matchesHtmlFilePath = path.posix.join('tmp', 'update', 'betcity', 'datesHtml', currentDateStr + '_' + first + '.html');
+              fs.writeFileSync(matchesHtmlFilePath, result);
+            });
+        });
+      }, Promise.resolve([]))
+
+      .then(function (results) {
+          return nightmare.end();
+      })
+
+      .catch(function (err) {
+        console.error(err);
+        process.exit(1);
+      });
     })
 
     .catch(function (err) {
