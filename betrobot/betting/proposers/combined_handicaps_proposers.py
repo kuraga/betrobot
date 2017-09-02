7fd6a66d9c8edeefd4a2d24186643fed77ecca10
Alexander Kurakin <kuraga333@mail.ru>
2017-09-02 23:56:39 +0300

    Combined handicaps: ставим на ставки 1, 1X, X2, 2, тоже

--- betrobot/betting/proposers/combined_handicaps_proposers.py
+++ betrobot/betting/proposers/combined_handicaps_proposers.py
@@ -5,6 +5,18 @@ from betrobot.betting.sport_util import filter_bets
 class CombinedHandicapsHomeProposer(Proposer):
 
     def _handle_bets(self, bets, match_header, combined_prediction, **kwargs):
+        bets_1_patterns = [ ('УГЛ', 'Исход', 'матч', '1') ]
+        bets_1 = filter_bets(bets_1_patterns, bets)
+        for bet in bets_1:
+            handicap = -0.5
+            self._handle_as_home_handicap(bet, handicap, combined_prediction, match_header, **kwargs)
+
+        bets_1X_patterns = [ ('УГЛ', 'Исход', 'матч', '1X') ]
+        bets_1X = filter_bets(bets_1X_patterns, bets)
+        for bet in bets_1X:
+            handicap = 0.5
+            self._handle_as_home_handicap(bet, handicap, combined_prediction, match_header, **kwargs)
+
         home_handicap_bets_patterns = [ ('УГЛ', 'Фора', 'матч', '1', '*') ]
         home_handicap_bets = filter_bets(home_handicap_bets_patterns, bets)
         for bet in home_handicap_bets:
@@ -29,6 +41,18 @@ class CombinedHandicapsHomeProposer(Proposer):
 class CombinedHandicapsAwayProposer(Proposer):
 
     def _handle_bets(self, bets, match_header, combined_prediction, **kwargs):
+        bets_X2_patterns = [ ('УГЛ', 'Исход', 'матч', 'X2') ]
+        bets_X2 = filter_bets(bets_X2_patterns, bets)
+        for bet in bets_X2:
+            handicap = 0.5
+            self._handle_as_away_handicap(bet, handicap, combined_prediction, match_header, **kwargs)
+
+        bets_2_patterns = [ ('УГЛ', 'Исход', 'матч', '2') ]
+        bets_2 = filter_bets(bets_2_patterns, bets)
+        for bet in bets_2:
+            handicap = -0.5
+            self._handle_as_away_handicap(bet, handicap, combined_prediction, match_header, **kwargs)
+
         away_handicap_bets_patterns = [ ('УГЛ', 'Фора', 'матч', '2', '*') ]
         away_handicap_bets = filter_bets(away_handicap_bets_patterns, bets)
         for bet in away_handicap_bets:
