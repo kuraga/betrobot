e161a39f036fb365f0dd83e93860e637f4bf149e
Alexander Kurakin <kuraga333@mail.ru>
2017-09-10 11:00:05 +0300

    Fix: очищаем коллекции перед предложением

--- scripts/propose/propose.sh
+++ scripts/propose/propose.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
 
 
+mongo betrobot --eval "db.prediction_infos.drop();"
+mongo betrobot --eval "db.proposed.drop();"
+
 find data/providers -type f -name "provider-*.pkl" -exec ./scripts/propose/propose.py "{}" \;
