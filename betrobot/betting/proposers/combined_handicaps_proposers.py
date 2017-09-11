9fcbdad35854e25159db232feb5d4f45ee714313
Alexander Kurakin <kuraga333@mail.ru>
2017-09-11 12:17:11 +0300

    Combined handicaps: берем по одной ставке

--- betrobot/betting/proposers/combined_handicaps_proposers.py
+++ betrobot/betting/proposers/combined_handicaps_proposers.py
@@ -1,3 +1,4 @@
+import numpy as np
 from betrobot.betting.proposer import Proposer
 from betrobot.betting.sport_util import filter_bets
 
@@ -5,20 +6,33 @@ from betrobot.betting.sport_util import filter_bets
 class CombinedHandicapsHomeProposerMixin:
 
     def _handle_bets(self, bets, match_header, prediction_info, **kwargs):
+        propositions = []
+
         bets_1 = filter_bets(self._bets_1_patterns, bets)
         for bet in bets_1:
             handicap = -0.5
-            self._handle_as_home_handicap(bet, handicap, prediction_info, match_header, **kwargs)
+            if self._handle_as_home_handicap(bet, handicap, prediction_info, match_header, **kwargs):
+                propositions.append(bet)
 
         bets_1X = filter_bets(self._bets_1X_patterns, bets)
         for bet in bets_1X:
             handicap = 0.5
-            self._handle_as_home_handicap(bet, handicap, prediction_info, match_header, **kwargs)
+            if self._handle_as_home_handicap(bet, handicap, prediction_info, match_header, **kwargs):
+                propositions.append(bet)
 
         home_handicap_bets = filter_bets(self._home_handicap_bets_patterns, bets)
         for bet in home_handicap_bets:
             handicap = bet['pattern'][4]
-            self._handle_as_home_handicap(bet, handicap, prediction_info, match_header, **kwargs)
+            if self._handle_as_home_handicap(bet, handicap, prediction_info, match_header, **kwargs):
+                propositions.append(bet)
+
+        # Оставляем одну ставку: с минимальным, но приемлимым коэффициентом
+        proposition_coeffs = [ bet['value'] for bet in propositions ]
+        big_proposition_coeffs = [ bet_value if bet_value >= 1.8 else np.nan for bet_value in proposition_coeffs ]
+        if not np.all(np.isnan(big_proposition_coeffs)):
+            good_bet_index = np.nanargmin(big_proposition_coeffs)
+            good_bet = propositions[good_bet_index]
+            self.propose(good_bet, match_header, prediction_info, **kwargs)
 
 
     def _handle_as_home_handicap(self, bet, handicap, prediction_info, match_header, **kwargs):
@@ -26,33 +40,45 @@ class CombinedHandicapsHomeProposerMixin:
         (home_number, away_number) = combined_prediction
 
         if home_number < -1:
-            return
+            return False
         if away_number > 1:
-            return
+            return False
 
-        if (handicap >= -1 and (home_number - away_number) >= 6) or \
+        return (handicap >= -1 and (home_number - away_number) >= 6) or \
           (handicap >= -0.5 and (home_number - away_number) >= 3) or \
-          (handicap >= 0 and (home_number - away_number) >= 0):
-            self.propose(bet, match_header, prediction_info, **kwargs)
+          (handicap >= 0 and (home_number - away_number) >= 0)
 
 
 class CombinedHandicapsAwayProposerMixin:
 
     def _handle_bets(self, bets, match_header, prediction_info, **kwargs):
+        propositions = []
+
         bets_X2 = filter_bets(self._bets_X2_patterns, bets)
         for bet in bets_X2:
             handicap = 0.5
-            self._handle_as_away_handicap(bet, handicap, prediction_info, match_header, **kwargs)
+            if self._handle_as_away_handicap(bet, handicap, prediction_info, match_header, **kwargs):
+                propositions.append(bet)
 
         bets_2 = filter_bets(self._bets_2_patterns, bets)
         for bet in bets_2:
             handicap = -0.5
-            self._handle_as_away_handicap(bet, handicap, prediction_info, match_header, **kwargs)
+            if self._handle_as_away_handicap(bet, handicap, prediction_info, match_header, **kwargs):
+                propositions.append(bet)
 
         away_handicap_bets = filter_bets(self._away_handicap_bets_patterns, bets)
         for bet in away_handicap_bets:
             handicap = bet['pattern'][4]
-            self._handle_as_away_handicap(bet, handicap, prediction_info, match_header, **kwargs)
+            if self._handle_as_away_handicap(bet, handicap, prediction_info, match_header, **kwargs):
+                propositions.append(bet)
+
+        # Оставляем одну ставку: с минимальным, но приемлимым коэффициентом
+        proposition_coeffs = [ bet['value'] for bet in propositions ]
+        big_proposition_coeffs = [ bet_value if bet_value >= 1.8 else np.nan for bet_value in proposition_coeffs ]
+        if not np.all(np.isnan(big_proposition_coeffs)):
+            good_bet_index = np.nanargmin(big_proposition_coeffs)
+            good_bet = propositions[good_bet_index]
+            self.propose(good_bet, match_header, prediction_info, **kwargs)
 
 
     def _handle_as_away_handicap(self, bet, handicap, prediction_info, match_header, **kwargs):
@@ -60,14 +86,13 @@ class CombinedHandicapsAwayProposerMixin:
         (home_number, away_number) = combined_prediction
 
         if away_number < -1:
-            return
+            return False
         if home_number > 1:
-            return
+            return False
 
-        if (handicap >= -1 and (away_number - home_number) >= 6) or \
+        return (handicap >= -1 and (away_number - home_number) >= 6) or \
           (handicap >= -0.5 and (away_number - home_number) >= 3) or \
-          (handicap >= 0 and (away_number - home_number) >= 0):
-            self.propose(bet, match_header, prediction_info, **kwargs)
+          (handicap >= 0 and (away_number - home_number) >= 0)
 
 
 class CombinedHandicapsHomeProposer(CombinedHandicapsHomeProposerMixin, Proposer):
